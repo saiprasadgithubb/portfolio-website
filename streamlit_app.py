bbc0d448b08fa961d10d6b50fe781ba882833548
saiprasadgithubb <134152278+saiprasadgithubb@users.noreply.github.com>
2024-04-21 09:36:25 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+st.title('My Portfolio')
+st.write("welcome to My Portfolio")
+st.text("Hello, welcome to my portfolio.A Passionate and dedicated Cyber Security student \n pursuing a bachelor of technology in computer science and cyber security engineer\n with the aim of to protect unauthorized access to computers and networks by using \n security tools.Iam proficient in python,c,java and have i used it for various \n  projects related to security,data encryption,data analysis and web scrap")
+st.header('projects')      
+ 
+st.subheader('Data Security approach on Cybercrime with web vulnerability')
+st.subheader('Bullynet:Unmasking cyber bullies on social networks')
+st.subheader('Rumuor Detection')
+st.text(" my project git hub link https://github.com/saiprasadgithubb/Rumour-Detection")
+st.subheader('linkedin:https://www.linkedin.com/in/sai-prasad-b10ab6258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')
+st.subheader('Github: https://github.com/saiprasadgithubb')
+
+
+
